b668ca1b3d45d27ef1fef6184c217b02e2ef499b
Adam <blekk2@gmail.com>
2015-06-30 23:50:21 +0200

    listbox renames

--- Source/Components/MainComponent.h
+++ Source/Components/MainComponent.h
@@ -22,7 +22,7 @@
 
 //[Headers]     -- You can add your own extra header files here --
 #include "JuceHeader.h"
-#include "ListBox.h"
+#include "ListBox.hpp"
 #include "Signals.hpp"
 
 namespace juce {
