6c889c2ccd234bac1174b3c3aa5e78b4af030a64
Adam <blekk2@gmail.com>
2017-04-23 17:55:44 +0200

    removed uneccessary implementation files

--- Source/Extensions/MusicPlayer/MusicPlayer.cpp
+++ Source/Extensions/MusicPlayer/MusicPlayer.cpp
@@ -1,25 +1,107 @@
 #include "MusicPlayer.h"
-#include "Formaters.h"
 #include "Logger.h"
-#include "MusicPlayerImplementation.h"
 
-using namespace jukebox;
+using namespace juce;
 using namespace jukebox::audio;
 
-MusicPlayer::MusicPlayer() :
-    pImpl(std::make_unique<MusicPlayerImplementation>())
+MusicPlayer::MusicPlayer()
+    : state(TransportState::Stopped)
 {
+    formatManager.registerBasicFormats();
+    transportSource.addChangeListener (this);
+
+    setAudioChannels (0, 2);
+}
+
+MusicPlayer::~MusicPlayer()
+{
+    shutdownAudio();
+}
+
+void MusicPlayer::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+    if (source == &transportSource)
+    {
+        if (transportSource.isPlaying())
+        {
+            changeState(TransportState::Playing);
+            LOG_INFO("Playing");
+        }
+        else
+        {
+            changeState(TransportState::Stopped);
+            LOG_INFO("Stopped");
+        }
+    }
+}
+
+void MusicPlayer::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay (samplesPerBlockExpected, sampleRate);
+}
+
+void MusicPlayer::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+void MusicPlayer::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    if (readerSource == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+
+    transportSource.getNextAudioBlock(bufferToFill);
 }
 
 void MusicPlayer::playSong(const std::string& song)
 {
-//    LOG_INFO("Playing: " << FillWithLeadingZeros(song.getAlbumNumber(), 3) << " " << FillWithLeadingZeros(song.getSongNumber(), 2));
-    LOG_INFO("Playing: " << song);
-    pImpl->playSong(song);
+    juce::File file(song);
+    if(!file.existsAsFile())
+    {
+        LOG_ERROR("File " << song << " does not exists! Skip playing");
+        return;
+    }
+
+    juce::AudioFormatReader* reader = formatManager.createReaderFor(file);
+    if (reader != nullptr)
+    {
+        std::unique_ptr<juce::AudioFormatReaderSource> newSource(std::make_unique<juce::AudioFormatReaderSource>(reader, true));
+        LOG_INFO("Sample rate: " << reader->sampleRate);
+        transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+        readerSource = std::move(newSource);
+        changeState(TransportState::Starting);
+    }
 }
 
 void MusicPlayer::stopPlaying()
 {
+    changeState(TransportState::Stopping);
     LOG_INFO("Stoped playing.");
-    pImpl->stopPlaying();
+}
+
+void MusicPlayer::changeState(TransportState newState)
+{
+    if (state == newState)
+        return;
+
+    state = newState;
+
+    switch (state)
+    {
+        case TransportState::Stopped:
+            finishedPlayingSignal();
+            transportSource.setPosition (0.0);
+            break;
+        case TransportState::Starting:
+            transportSource.start();
+            break;
+        case TransportState::Playing:
+            break;
+        case TransportState::Stopping:
+            transportSource.stop();
+            break;
+    }
 }
--- Source/Extensions/MusicPlayer/MusicPlayer.h
+++ Source/Extensions/MusicPlayer/MusicPlayer.h
@@ -3,22 +3,46 @@
 
 #include <memory>
 #include "IMusicPlayer.h"
+#include "JuceHeader.h"
 
 namespace jukebox { namespace audio {
-  
-class MusicPlayer : public IMusicPlayer
+
+enum class TransportState
+{
+    Stopped,
+    Starting,
+    Playing,
+    Stopping
+};
+
+class MusicPlayer : public juce::ChangeListener,
+                    public juce::AudioAppComponent,
+                    public IMusicPlayer
 {
 public:
     MusicPlayer();
+    ~MusicPlayer();
+
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+
+    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override;
+    void releaseResources() override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
 
     void playSong(const std::string&) override;
-
     void stopPlaying() override;
-    
+
 private:
-    std::unique_ptr<IMusicPlayer> pImpl;
+    void changeState(TransportState newState);
+
+    TransportState state;
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+
+
 };
 
 }}
-    
+
 #endif  // MUSICPLAYER_H_INCLUDED
