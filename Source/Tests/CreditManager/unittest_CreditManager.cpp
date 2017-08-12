6103ded4b39c8d01adfdf40aeee968cadb1c8388
Adam <blekk2@gmail.com>
2017-08-12 18:54:22 +0200

    added more unit tests to credit manager

--- Source/Tests/CreditManager/unittest_CreditManager.cpp
+++ Source/Tests/CreditManager/unittest_CreditManager.cpp
@@ -9,10 +9,16 @@ protected:
     CreditManager creditManager;
 };
 
+namespace {
+    constexpr unsigned zeroCredit(0);
+    constexpr unsigned oneSongCredit(1);
+    constexpr unsigned threeSongsCredit(3);
+    constexpr unsigned oneAlbumCredit(6);
+}
+
 TEST_F(CreditManagerTest, empty)
 {
-    unsigned int expected(0);
-    EXPECT_EQ(expected, creditManager.getCredits());
+    EXPECT_EQ(zeroCredit, creditManager.getCredits());
     EXPECT_FALSE(creditManager.hasEnoughCreditsToPlaySong());
     EXPECT_FALSE(creditManager.hasEnoughCreditsToPlayAlbum());
     EXPECT_FALSE(creditManager.startPlaySong());
@@ -22,22 +28,65 @@ TEST_F(CreditManagerTest, empty)
 TEST_F(CreditManagerTest, insertSingle50)
 {
     creditManager.coinInsert50();
-    unsigned int expected(1);
-    EXPECT_EQ(expected, creditManager.getCredits());
+
+    EXPECT_EQ(oneSongCredit, creditManager.getCredits());
+    EXPECT_TRUE(creditManager.hasEnoughCreditsToPlaySong());
 }
 
 TEST_F(CreditManagerTest, insertSingle100)
 {
     creditManager.coinInsert100();
 
-    unsigned int expected(3);
-    EXPECT_EQ(expected, creditManager.getCredits());
+    EXPECT_EQ(threeSongsCredit, creditManager.getCredits());
+    EXPECT_FALSE(creditManager.hasEnoughCreditsToPlayAlbum());
 }
 
 TEST_F(CreditManagerTest, insertSingle200)
 {
     creditManager.coinInsert200();
 
-    unsigned int expected(6);
-    EXPECT_EQ(expected, creditManager.getCredits());
+    EXPECT_EQ(oneAlbumCredit, creditManager.getCredits());
+    EXPECT_TRUE(creditManager.hasEnoughCreditsToPlayAlbum());
+}
+
+TEST_F(CreditManagerTest, notEnoughCreditsToPlayAlbum_tooFewInserted)
+{
+    creditManager.coinInsert100();
+    creditManager.coinInsert50();
+
+    EXPECT_FALSE(creditManager.hasEnoughCreditsToPlayAlbum());
+}
+
+TEST_F(CreditManagerTest, creditIncrease_empty)
+{
+    creditManager.creditIncrease();
+
+    EXPECT_EQ(oneSongCredit, creditManager.getCredits());
+}
+
+TEST_F(CreditManagerTest, creditIncrease_afterInsertion)
+{
+    creditManager.coinInsert50();
+    creditManager.coinInsert100();
+
+    creditManager.creditIncrease();
+
+    EXPECT_EQ(oneAlbumCredit - 1, creditManager.getCredits());
+}
+
+TEST_F(CreditManagerTest, creditDecrease_empy)
+{
+    creditManager.creditDecrease();
+
+    EXPECT_EQ(zeroCredit, creditManager.getCredits());
+}
+
+TEST_F(CreditManagerTest, creditDecrease_afterInsertion)
+{
+    creditManager.coinInsert200();
+    creditManager.coinInsert50();
+
+    creditManager.creditDecrease();
+
+    EXPECT_EQ(oneAlbumCredit, creditManager.getCredits());
 }
